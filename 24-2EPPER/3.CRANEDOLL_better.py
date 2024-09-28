236010f320321fab8f4ab579ed1bb4469f4ba229
HyunKyungHan <hhk@ewhain.net>
2024-09-28 10:57:13 +0900

    [스택] 프로그래머스

--- 24-2EPPER/3.CRANEDOLL_better.py
+++ 24-2EPPER/3.CRANEDOLL_better.py
@@ -1,23 +1,24 @@
-def solution(board, moves):
+def get_doll(board, moves):
     stacklist = []
     answer = 0
-
-    for i in moves:
-        for j in range(len(board)):
-            if board[j][i-1] != 0:
-                stacklist.append(board[j][i-1])
+    
+    for i in moves: # 크레인 좌표들을 하나씩 iterate
+        for j in range(len(board)): # 행의 수
+            if board[j][i-1] != 0: # 열을 고정, 행을 하나씩 아래로 움직이면서 탐색
+                stacklist.append(board[j][i-1]) # 비어있지 않다면 stacklist에 추가
                 board[j][i-1] = 0
-
+                
                 if len(stacklist) > 1:
                     if stacklist[-1] == stacklist[-2]:
                         stacklist.pop(-1)
                         stacklist.pop(-1)
-                        answer += 2     
+                        answer += 2
                 break
-
     return answer
 
-if __name__ == '__main__':
+def solution(board, moves):
     board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
     moves = [1,5,3,5,1,2,1,4]
-    print(solution(board, moves))
+    ans = 0
+    ans = get_doll(board, moves)
+    return ans
